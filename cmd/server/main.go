41fd34435e366141edbae182b3000f97bbbb2d52
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-11-25 14:50:04 +0100

    feat: handling interup signals

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,107 @@
 package main
 
 import (
+	"canvas/server"
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+func start() int {
+	logEnv := getStringOrDefaultValue("LOG_ENV", "development")
+
+	log, err := createLogger(logEnv)
+	if err != nil {
+		fmt.Println("Error setting up the logger", err)
+		return 1
+	}
+
+	defer func() {
+		_ = log.Sync()
+	}()
+
+	host := getStringOrDefaultValue("HOST", "localhost")
+	port := getIntOrDefaultValue("PORT", 8080)
+	s := server.New(server.Options{Host: host, Port: port})
+
+	// That function makes sure to cancel the returned context if it receives one of the signals we have asked for.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+
+	defer stop()
+
+	// An error group is something that can run functions in goroutines, wait for all the functions to finish, and return any errors.
+
+	// The Context returned by the errgroup.WithContext function is cancelled the first time any of the functions return an error,
+	// which enables us to shut down immediately if there's an error starting up.
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// We then call eg.Go with a small function which starts the server and returns any error
+	eg.Go(func() error {
+		if err := s.Start(); err != nil {
+			log.Info("Error starting server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	// After that, we block until the ctx from before is cancelled, by reading from the channel returned by ctx.Done.
+	<-ctx.Done()
+
+	// hen it is cancelled (either by a signal or a function passed to eg.Go returning an error),
+	// we call Stop on our server in another goroutine passed to the error group.
+	eg.Go(func() error {
+		if err := s.Stop(); err != nil {
+			log.Info("Error starting server", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
+		return 1
+	}
+	return 0
+}
+
+func createLogger(env string) (*zap.Logger, error) {
+	switch env {
+	case "production":
+		return zap.NewProduction()
+	case "developement":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+
+}
+
+// Get Env Variables
+func getStringOrDefaultValue(name, defaultV string) string {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	return v
+}
+
+func getIntOrDefaultValue(name string, defaultV int) int {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	vAsInt, err := strconv.Atoi(v)
+	if err != nil {
+		return defaultV
+	}
+	return vAsInt
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
 module canvas
 
-go 1.18
+go 1.24.0
 
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.2.3
 )
 
-require github.com/matryer/is v1.4.1 // indirect
+require (
+	github.com/matryer/is v1.4.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+)
